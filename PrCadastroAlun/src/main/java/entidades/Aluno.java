3d55ac5ac96157c1ee28602d2829143d1374bc76
deco_ <deco_@DESKTOP-GT8E8OO>
2018-10-06 19:22:53 -0300

    Commit #1

---
+++ PrCadastroAlun/src/main/java/entidades/Aluno.java
@@ -0,0 +1,33 @@
+package  entidades;
+
+public class Aluno {
+	private int telefone;
+	private String ra;
+	private String nome;
+	private String sexo;
+	
+	public String getRA() {
+		return ra;
+	}
+	public void setRA(String ra) {
+		this.ra = ra;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+}
